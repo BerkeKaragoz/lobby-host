607b74f234ce11f4422a8e17b52e09e92a1edfe8
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2022-05-07 18:02:11 +0300

    Initial configuration

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,12 @@
 import * as React from "react";
-export const Button = () => {
-  return <button>Boop</button>;
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {};
+
+const Button: React.FC<Props> = (props) => {
+  const { ...rest } = props;
+
+  return <button {...rest} />;
 };
+
+export type { Props as ButtonProps };
+export default Button;
