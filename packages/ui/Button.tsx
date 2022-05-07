7c48d4e1b06c0769dc39e532f0b57a4aed6ded47
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2022-05-07 19:41:36 +0300

    feat: add storybook

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,11 +1,28 @@
 import * as React from "react";
 
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {};
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+};
 
 const Button: React.FC<Props> = (props) => {
   const { ...rest } = props;
 
-  return <button {...rest} />;
+  return <button style={{ background: "red" }} {...rest} />;
 };
 
 export type { Props as ButtonProps };
